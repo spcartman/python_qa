64eaa4ef15d53a9d497495f7e3357f2e3babb956
spcartman <spcartman@tut.by>
2016-10-07 18:48:22 +0300

    ContactHelper created.

--- fixture/application.py
+++ fixture/application.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.support.ui import Select
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
 
 
 class Application:
@@ -11,6 +11,7 @@ class Application:
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def open_home_page(self):
         wd = self.wd
@@ -19,86 +20,6 @@ class Application:
     def return_home(self):
         wd = self.wd
         wd.find_element_by_link_text("home page").click()
-
-    def create_contact(self, contact):
-        wd = self.wd
-        # open contact creation form
-        wd.find_element_by_link_text("add new").click()
-        # fill contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.fname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.mname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nick)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address1)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.hphone)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mphone)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.wphone)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact.fax)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email1)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact.email3)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(contact.homepage)
-        b_day = Select(wd.find_element_by_name("bday"))
-        b_day.select_by_value(contact.bday)
-        d_month = Select(wd.find_element_by_name("bmonth"))
-        d_month.select_by_value(contact.bmonth)
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.byear)
-        a_day = Select(wd.find_element_by_name("aday"))
-        a_day.select_by_value(contact.aday)
-        a_month = Select(wd.find_element_by_name("amonth"))
-        a_month.select_by_value(contact.amonth)
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(contact.ayear)
-        new_group = Select(wd.find_element_by_name("new_group"))
-        new_group.select_by_index(contact.group)
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contact.address2)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(contact.hphone2)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(contact.notes)
-        # submit contact creation
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        # check that contact was created
-        self.return_home()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -15,11 +15,11 @@ def app(request):
 def test_add_contact(app):
     app.open_home_page()
     app.session.login('admin', 'secret')
-    app.create_contact(Contact(fname="First Name 001", mname="Middle Name", lname="Last Name 001",
-                                nick="fooname", title="Mr.", company="MacroSoft", address1="6, Wood road 19",
-                                hphone="185 83 71", mphone="378 74 28", wphone="479 82 21", fax="877 63 76",
-                                email1="foo@mail.coom", email2="foo2@mail.coom", email3="foo3@mail.com",
-                                homepage="http://localhost/", bday="18", bmonth="July", byear="1981", aday="7",
-                                amonth="October", ayear="1983", group=6, address2="8, Home ave 24",
-                                hphone2="785 23 67", notes="The contact is created by the script."))
+    app.contact.create(Contact(fname="First Name 001", mname="Middle Name", lname="Last Name 001",
+                               nick="fooname", title="Mr.", company="MacroSoft", address1="6, Wood road 19",
+                               hphone="185 83 71", mphone="378 74 28", wphone="479 82 21", fax="877 63 76",
+                               email1="foo@mail.coom", email2="foo2@mail.coom", email3="foo3@mail.com",
+                               homepage="http://localhost/", bday="18", bmonth="July", byear="1981", aday="7",
+                               amonth="October", ayear="1983", group=6, address2="8, Home ave 24",
+                               hphone2="785 23 67", notes="The contact is created by the script."))
     app.session.logout()
