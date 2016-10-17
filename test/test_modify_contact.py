1a4035c8a12877575265456599210c477b430cff
spcartman <spcartman@tut.by>
2016-10-17 16:41:25 +0300

    Smart contact modification is implemented.

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,6 +4,8 @@ from model.contact import Contact
 
 def test_modify_whole_contact(app):
     app.navigation.go_home()
+    if app.contact.count() == 0:
+        app.contact.create(Contact(fname="Safety Contact"))
     app.contact.modify(Contact(fname="New First Name 003", mname="New Middle Name", lname="New Last Name 003",
                                nick="New fooname", title="New Mr.", company="New MacroSoft", address1="6, New road 19",
                                hphone="333 83 33", mphone="333 74 33", wphone="333 82 33", fax="333 63 33",
@@ -16,5 +18,7 @@ def test_modify_whole_contact(app):
 
 def test_modify_contact_name(app):
     app.navigation.go_home()
+    if app.contact.count() == 0:
+        app.contact.create(Contact(fname="Safety Contact"))
     app.contact.modify(Contact(fname="New First Name 003"))
     app.navigation.go_home()
