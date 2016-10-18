8afb0a1115057ead490e98cb5dac6c5100e23472
spcartman <spcartman@tut.by>
2016-10-19 01:49:03 +0300

    Bug is fixed. Added correct navigation after precondition is fired.

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -6,5 +6,6 @@ def test_del_group(app):
     app.navigation.open_groups_page()
     if app.count_item() == 0:
         app.group.create(Group(name="Safety Group"))
+        app.navigation.open_groups_page()
     app.group.delete()
     app.navigation.open_groups_page()
