5c1f695e7807c4500ed99d803e982ba0c0c5507e
spcartman <spcartman@tut.by>
2016-10-20 03:28:50 +0300

    Add tests were updated with old and new lists comparison.

--- model/contact.py
+++ model/contact.py
@@ -1,3 +1,6 @@
+from sys import maxsize
+
+
 class Contact:
 
     def __init__(self, fname=None, mname=None, lname=None, nick=None, title=None, company=None, address1=None,
@@ -36,4 +39,11 @@ class Contact:
         return '%s:%s:%s' % (self.id, self.fname, self.lname)
 
     def __eq__(self, other):
-        return self.id == other.id and self.fname == other.fname and self.lname == other.lname
+        return (self.id is None or other.id is None or self.id == other.id) and self.fname == other.fname and \
+               self.lname == other.lname
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,13 +5,15 @@ from model.contact import Contact
 def test_add_contact(app):
     app.navigation.go_home()
     old_contacts = app.contact.get_contact_list()
-    app.contact.create(Contact(fname="First Name 005", mname="Middle Name", lname="Last Name 005",
-                               nick="fooname", title="Mr.", company="MacroSoft", address1="6, Wood road 19",
-                               hphone="185 83 71", mphone="378 74 28", wphone="479 82 21", fax="877 63 76",
-                               email1="foo@mail.coom", email2="foo2@mail.coom", email3="foo3@mail.com",
-                               homepage="http://localhost/", bday="18", bmonth="July", byear="1981", aday="7",
-                               amonth="October", ayear="1983", group=6, address2="8, Home ave 24",
-                               hphone2="785 23 67", notes="The contact is created by the script."))
+    contact = Contact(fname="First Name 005", mname="Middle Name", lname="Last Name 005", nick="fooname",
+                      title="Mr.", company="MacroSoft", address1="6, Wood road 19", hphone="185 83 71",
+                      mphone="378 74 28", wphone="479 82 21", fax="877 63 76", email1="foo@mail.coom",
+                      email2="foo2@mail.coom", email3="foo3@mail.com", homepage="http://localhost/", bday="18",
+                      bmonth="July", byear="1981", aday="7", amonth="October", ayear="1983", group=6,
+                      address2="8, Home ave 24", hphone2="785 23 67", notes="The contact is created by the script.")
+    app.contact.create(contact)
     app.navigation.go_home()
     new_contacts = app.contact.get_contact_list()
     assert (len(old_contacts) + 1) == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
