89d18a298b54d7758f70a3fce2893b48c01b1f30
spcartman <spcartman@tut.by>
2016-11-05 17:18:30 +0300

    Added directory support.

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import json
+import os.path
 from fixture.application import Application
 
 
@@ -12,8 +13,9 @@ def app(request):
     global fixture
     global target
     if target is None:
-        with open(request.config.getoption("--target")) as config_file:
-            target = json.load(config_file)
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
     if (fixture is None) or (not fixture.is_valid()):
         fixture = Application(browser=request.config.getoption("--browser"), base_url=target["baseUrl"])
         fixture.navigation.open_home_page()
