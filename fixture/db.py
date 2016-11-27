82b9d4e9e1c1647a252b81ec9465ea919a5b74f3
spcartman <spcartman@tut.by>
2016-11-27 16:08:24 +0300

    Add group tests are implemented with DB checks.

--- fixture/db.py
+++ fixture/db.py
@@ -1,4 +1,5 @@
-import pymysql
+import mysql.connector
+from model.group import Group
 
 
 class DBFixture:
@@ -8,7 +9,20 @@ class DBFixture:
         self.db_name = db_name
         self.user = user
         self.password = password
-        self.connection = pymysql.connect(host=host, database=db_name, user=user, password=password)
+        self.connection = mysql.connector.connect(host=host, database=db_name, user=user, password=password)
+        self.connection.autocommit = True
+
+    def get_group_list(self):
+        group_list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("SELECT group_id, group_name, group_header, group_footer FROM group_list")
+            for row in cursor:
+                (id, name, header, footer) = row
+                group_list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return group_list
 
     def destroy(self):
         self.connection.close()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,13 +2,12 @@
 from model.group import Group
 
 
-def test_add_group(app, data_groups):
+def test_add_group(app, db, data_groups):
     group = data_groups
     app.navigation.open_groups_page()
-    old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
     app.group.create(group)
     app.navigation.open_groups_page()
-    assert (len(old_groups) + 1) == app.count_item()
-    new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
