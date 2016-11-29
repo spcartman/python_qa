8bab66c4abc6bc2740d2f7efc075f847d999d729
spcartman <spcartman@tut.by>
2016-11-29 16:39:36 +0300

    Methods to determine contacts in group and not in group are implemented.

--- fixture/orm.py
+++ fixture/orm.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from pony.orm import *
-from pymysql.converters import decoders
+from pymysql.converters import encoders, decoders, convert_mysql_timestamp
 from model.group import Group
 from model.contact import Contact
 
@@ -15,6 +15,8 @@ class ORMFixture:
         name = Optional(str, column="group_name")
         header = Optional(str, column="group_header")
         footer = Optional(str, column="group_footer")
+        contacts = Set(lambda: ORMFixture.ORMContact, table="address_in_groups", column="id", reverse="groups",
+                       lazy=True)
 
     class ORMContact(db.Entity):
         _table_ = "addressbook"
@@ -22,9 +24,14 @@ class ORMFixture:
         firstname = Optional(str, column="firstname")
         lastname = Optional(str, column="lastname")
         deprecated = Optional(datetime, column="deprecated")
+        groups = Set(lambda: ORMFixture.ORMGroup, table="address_in_groups", column="group_id", reverse="contacts",
+                     lazy=True)
 
     def __init__(self, host, db_name, user, password):
-        self.db.bind("mysql", host=host, database=db_name, user=user, password=password, conv=decoders)
+        conv = encoders
+        conv.update(decoders)
+        conv[datetime] = convert_mysql_timestamp
+        self.db.bind("mysql", host=host, database=db_name, user=user, password=password, conv=conv)
         self.db.generate_mapping()
         sql_debug(True)
 
@@ -44,5 +51,17 @@ class ORMFixture:
 
     @db_session
     def get_contact_list(self):
-        return self.covert_contacts_to_model(select(c for c in ORMFixture.ORMContact
-                                                    if c.deprecated is None))
+        return self.covert_contacts_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
+
+    @db_session
+    def get_group_by_id(self, group):
+        return list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+
+    @db_session
+    def get_contacts_in_group(self, group):
+        return self.covert_contacts_to_model(self.get_group_by_id(group).contacts)
+
+    @db_session
+    def get_contacts_not_in_group(self, group):
+        return self.covert_contacts_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None
+                                                    and self.get_group_by_id(group) not in c.groups))
--- test/playground.py
+++ test/playground.py
@@ -14,12 +14,13 @@
 #     conn.close()
 
 from fixture.orm import ORMFixture
+from model.group import Group
 
 
 db = ORMFixture(host="127.0.0.1", db_name="addressbook", user="root", password="")
 
 try:
-    l = db.get_contact_list()
+    l = db.get_contacts_not_in_group(Group(id="161"))
     for i in l:
         print(i)
     print("\n", len(l))
