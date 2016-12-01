cb4e9b3437a953c8d6e1009455f207a3952b4e07
spcartman <spcartman@tut.by>
2016-12-01 23:08:13 +0300

    Allure reports support is added. Add contacts test is reworked to support BDD like style.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
 
 
 def test_add_contact(app, db, data_contacts, check_ui):
     contact = data_contacts
-    app.navigation.go_home()
-    old_contacts = db.get_contact_list()
-    app.contact.create(contact)
-    old_contacts.append(contact)
-    new_contacts = db.get_contact_list()
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-    if check_ui:
+    with pytest.allure.step("Given a contact list"):
         app.navigation.go_home()
-        assert sorted(map(app.contact.strip_spaces, new_contacts),
-                      key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+        old_contacts = db.get_contact_list()
+    with pytest.allure.step("When I add the contact %s to the list" % contact):
+        app.contact.create(contact)
+    with pytest.allure.step("Then the new contact list is equal to the old list with the added contact"):
+        old_contacts.append(contact)
+        new_contacts = db.get_contact_list()
+        assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+        if check_ui:
+            app.navigation.go_home()
+            assert sorted(map(app.contact.strip_spaces, new_contacts),
+                          key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
